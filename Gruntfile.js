0a3c13d9f13031e892a128834fbf557f960d61de
Thomas Esterer <esterer.thomas@gmail.com>
2014-02-05 15:28:07 +0100

    preparing deploy to heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -403,4 +403,8 @@ module.exports = function (grunt) {
     'test',
     'build'
   ]);
+
+  grunt.registerTask('heroku:development', 'clean less mincss');
+
+  grunt.registerTask('heroku:production', 'clean less mincss uglify');
 };
